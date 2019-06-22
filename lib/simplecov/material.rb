313c7d80875702b52090470558e3869a6ee077b8
Christopher Pezza <pezza.chris@gmail.com>
2019-06-21 23:59:09 -0400

    initial creation of gem

---
+++ lib/simplecov/material.rb
@@ -0,0 +1,8 @@
+require "simplecov/material/version"
+
+module Simplecov
+  module Material
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/simplecov/material/version.rb
@@ -0,0 +1,5 @@
+module Simplecov
+  module Material
+    VERSION = "0.1.0"
+  end
+end
