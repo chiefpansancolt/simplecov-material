409f22076a44ae2692b390bba9f25ff135e415e8
Christopher Pezza <pezza.chris@gmail.com>
2019-07-19 22:26:32 -0400

    Version bump to 0.1.0

--- lib/simplecov-material/version.rb
+++ lib/simplecov-material/version.rb
@@ -1,9 +1,4 @@
 # frozen_string_literal: true
-
-module SimpleCov
-  module Formatter
-    class MaterialFormatter
-      VERSION = "0.0.0"
-    end
-  end
+module SimplecovMaterial
+  VERSION = "0.1.0"
 end
