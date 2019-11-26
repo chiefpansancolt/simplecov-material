2a88cd5ef6f29dda400b65c12e58cca97c937d6e
Christopher Pezza <pezza.chris@gmail.com>
2019-11-25 21:36:15 -0500

    upgrade to mdc table and add sortability

--- dist/scripts/table.js
+++ dist/scripts/table.js
@@ -1,3 +1,7 @@
+import {MDCDataTable} from '@material/data-table';
+const dataTable = new MDCDataTable(document.querySelector('.mdc-data-table'));
+
+
 export function searchTable(elementId) {
   const input = document.getElementById(elementId);
   const filter = input.value.toUpperCase();
@@ -16,3 +20,64 @@ export function searchTable(elementId) {
 }
 
 window.searchTable = searchTable;
+
+function sortTable(table, col, reverse) {
+    var tb = table.tBodies[0],
+        tbh = table.tHead,
+        th = Array.prototype.slice.call(tbh.rows, 0),
+        tr = Array.prototype.slice.call(tb.rows, 0),
+        i, c;
+    reverse = -((+reverse) || -1);
+    tr = tr.sort(function (a, b) {
+        return reverse
+            * (a.cells[col].textContent.trim()
+                .localeCompare(b.cells[col].textContent.trim())
+               );
+    });
+    for(i = 0; i < th.length; ++i) {
+      for(c = 0; c < th[i].cells.length; ++c) {
+        if(c != col) {
+          th[i].cells[c].childNodes[1].classList.add('hide');
+          th[i].cells[c].childNodes[3].classList.add('hide');
+        } else {
+          if(reverse == 1) {
+            th[i].cells[c].childNodes[1].classList.add('hide');
+            th[i].cells[c].childNodes[3].classList.remove('hide');
+          } else {
+            th[i].cells[c].childNodes[1].classList.remove('hide');
+            th[i].cells[c].childNodes[3].classList.add('hide');
+          }
+        }
+      }
+    }
+    for(i = 0; i < tr.length; ++i) tb.appendChild(tr[i]);
+}
+
+function makeSortable(table) {
+    var th = table.tHead, i;
+    th && (th = th.rows[0]) && (th = th.cells);
+    if (th) i = th.length;
+    else return;
+    while (--i >= 0) (function (i) {
+        var dir = 1;
+        th[i].addEventListener('click', function () {sortTable(table, i, (dir = 1 - dir))});
+    }(i));
+}
+
+function makeAllSortable(parent) {
+    parent = parent || document.body;
+    var t = parent.getElementsByTagName('table'), i = t.length;
+    while (--i >= 0) makeSortable(t[i]);
+}
+
+function defaultSort() {
+  var t = document.getElementsByTagName('table'), i;
+  for (i = 0; i < t.length; ++i) {
+    sortTable(t[i], 1, -1);
+  }
+}
+
+window.onload = function () {
+  makeAllSortable();
+  defaultSort();
+};
